c0cd1bd0d48126c9f15aca198f50a697017795fb
Fabricio Valerio <fvr.valerio@gmail.com>
2019-07-16 21:56:31 -0400

    Avatar1

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,10 +3,10 @@ import crypto from 'crypto';
 import { extname, resolve } from 'path';
 
 export default {
-  Storage: multer.diskStorage({
+  storage: multer.diskStorage({
     destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
     filename: (req, file, cb) => {
-      crypto.randomBytes(16, (req, file, cb) => {
+      crypto.randomBytes(16, (err, res) => {
         if (err) return cb(err);
 
         return cb(null, res.toString('hex') + extname(file.originalname));
